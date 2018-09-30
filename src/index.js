4bb70b642c63e650f68933f8b2a64f77fe36aeb4
Yana Bashtyk <Yana_bashtyk@epam.com>
2018-09-30 03:28:30 +0300

    solution longest sequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  const sequenceObj = new Set ();
+  let length = 0;
+  
+  for (let i=0, len = array.length; i<len; i++){
+    sequenceObj.add(array[i]);
+  }
+  
+  for (let num=0, len = array.length; num < len; num++){
+    //if element is the starting elem of a sequence
+    if(!(sequenceObj.has(array[num]-1))) {
+      //check for the next element in sequence
+      let current = array[num];
+      while (sequenceObj.has(current)){
+        current++;
+      }
+      length = Math.max(length, current - array[num]);
+    }
+  }
+    
+  return length;
 }
